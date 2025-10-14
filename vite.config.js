4e48177c5dec62086d32ac81c5282a338c9292c1
raw724 <russellwalton99@icloud.com>
2025-10-14 17:20:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './' // <-- must be inside the object, no extra braces
+  plugins: [react()]
 })
