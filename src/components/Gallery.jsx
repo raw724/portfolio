b5a558b7f3504e027aca96f2f8e8718e03682dd8
raw724 <russellwalton99@icloud.com>
2025-10-17 22:23:51 +0100

    Replace gallery images with new image paths
    Updated the image sources in the Gallery component.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 
 export default function Gallery() {
   const images = [
-    '/gallery1.jpg',
-    '/gallery2.jpg',
-    '/gallery3.jpg',
-    '/gallery4.jpg',
-    '/gallery5.jpg',
-    '/gallery6.jpg',
+    '/20220421_153956_iOS.jpg',
+'/20220421_154006_iOS.jpg',
+'/20220421_154012_iOS.jpg',
+'/20220421_154017_iOS.jpg',
+'/20220421_154022_iOS.jpg',
+'/20220421_154028_iOS.jpg',
+'/20220421_154033_iOS.jpg',
+'/20220421_154038_iOS.jpg',
+'/20220421_154043_iOS.jpg',
+'/20220421_154048_iOS.jpg',
+'/20220421_154053_iOS.jpg',
+'/20220421_154058_iOS.jpg',
+'/20220421_154103_iOS.jpg',
+'/20220421_154108_iOS.jpg',
+'/20220421_154113_iOS.jpg',
+'/20220421_154118_iOS.jpg',
+'/20220421_154123_iOS.jpg',
+'/20220421_154128_iOS.jpg',
+'/20220421_154133_iOS.jpg',
+'/20220421_154138_iOS.jpg'
+
   ];
 
   return (
