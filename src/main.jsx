4246e1369826986a81aed62c742c33fdc37d9ecb
raw724 <russellwalton99@icloud.com>
2025-10-19 17:23:11 +0100

    Add comment to Gallery route in main.jsx
    Added comment to clarify the Gallery route.

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/gallery" element={<Gallery />} /> {/* Gallery is a separate route */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
