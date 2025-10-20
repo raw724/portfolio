8ef153fc469450baa9a899109f8e9b00ccc1d34f
raw724 <russellwalton99@icloud.com>
2025-10-20 11:22:42 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
-import Gallery from './components/Gallery.jsx';
+//import Gallery from './components/Gallery.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
